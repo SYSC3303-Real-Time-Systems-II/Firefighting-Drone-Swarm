2c929e2a49d4ac6feff07d9eea26d4ab0c821049
ranveerdhaliwal <ranveerdhaliwal.03@gmail.com>
2025-01-26 01:48:05 -0500

    1.Updated classes to implement runnable 2.FireIncidentSubsystem objs can read input events and create an InputEvent obj

--- DroneSubsystem.java
+++ DroneSubsystem.java
@@ -1,2 +1,15 @@
-public class DroneSubsystem {
+public class DroneSubsystem implements Runnable{
+
+    public String name;
+
+    public DroneSubsystem(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("["+name +"] - "+ i);
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello");
+
+        FireIncidentSubsystem fis1 = new FireIncidentSubsystem("FIS", "test.txt");
+
+//        Thread fis1_t1 = new Thread(fis1);
+//
+//        DroneSubsystem drone = new DroneSubsystem("Drone");
+//        Thread drone_t1 = new Thread(drone);
+//
+//        Scheduler scheduler = new Scheduler("Scdlr");
+//        Thread scheduler_t1 = new Thread(scheduler);
+//
+//
+//
+//        fis1_t1.start();
+//        drone_t1.start();
+//        scheduler_t1.start();
     }
 
 }
