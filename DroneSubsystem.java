d3c14f5b7a670c8602491648506f01d027de2295
ranveerdhaliwal <ranveerdhaliwal.03@gmail.com>
2025-01-27 00:36:29 -0500

    Created first draft of iter1

--- DroneSubsystem.java
+++ DroneSubsystem.java
@@ -1,15 +1,45 @@
+import java.util.ArrayList;
+import java.util.Dictionary;
+
 public class DroneSubsystem implements Runnable{
 
-    public String name;
+    private String name;
+    private Systems systemType;
+    public MessageRelay relay;
 
-    public DroneSubsystem(String name) {
+    public DroneSubsystem(String name,MessageRelay relay) {
         this.name = name;
+        this.systemType = Systems.DroneSubsystem;
+        this.relay = relay;
+    }
+
+    public RelayPackage createRelayPackage(String packageID,InputEvent inputEvent, Boolean finalEvent){
+        return new RelayPackage(packageID,Systems.Scheduler,inputEvent, finalEvent);
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("["+name +"] - "+ i);
+        ArrayList<RelayPackage> outgoingPackages = new ArrayList<RelayPackage>();
+
+        //listen for packages from Scheduler
+        while (true) {
+            RelayPackage receivedPackage = relay.get(name, systemType);
+            System.out.println("[" + name + "] --"+receivedPackage);
+            outgoingPackages.add(receivedPackage);
+            System.out.println("[" + name + "] -- Processing event package: " + receivedPackage);
+            if (receivedPackage.lastPackage){
+                break;
+            }
         }
+
+        //send packages to Scheduler
+        for (int i = 0; i < outgoingPackages.size(); i++) {
+            String packageID = name+"_"+i;
+            RelayPackage sendingPackge = outgoingPackages.get(i);
+            RelayPackage relayPackage = createRelayPackage( packageID, sendingPackge.event, sendingPackge.lastPackage);
+            System.out.println("[" + name + "] -- created " + packageID +" to send to Scheduler");
+            relay.put(name, relayPackage);
+        }
+
     }
 }
--- FireIncidentSubsystem.java
+++ FireIncidentSubsystem.java
@@ -1,24 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class FireIncidentSubsystem implements Runnable {
 
     private String name;
+    private Systems systemType;
     private ArrayList<InputEvent> inputEvents;
+    public MessageRelay relay;
 
-    public FireIncidentSubsystem(String name, String inputFileName){
+
+    public FireIncidentSubsystem(String name, String inputFileName,MessageRelay relay){
         this.name = name;
+        this.systemType = Systems.FireIncidentSubsystem;
         this.inputEvents = readInput(inputFileName);
-
-        for(int i=0; i < inputEvents.size(); i++){
-            InputEvent a = inputEvents.get(i);
-            System.out.println(a.toString());
-        }
-
+        this.relay = relay;
     }
-
 
     public ArrayList<InputEvent> readInput(String inputFileName){
         ArrayList<InputEvent> inputEvents = new ArrayList<InputEvent>();
@@ -40,10 +40,41 @@ public class FireIncidentSubsystem implements Runnable {
         return null;
     }
 
+    public RelayPackage createRelayPackage(String packageID,InputEvent inputEvent, Boolean finalEvent){
+        return new RelayPackage(packageID,Systems.Scheduler,inputEvent, finalEvent);
+    }
+
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("["+name +"] - "+ i);
+
+        ArrayList<RelayPackage> acknowledgePackages = new ArrayList<RelayPackage>();
+        for (int i = 0; i < inputEvents.size(); i++) {
+
+            Boolean finalEvent = false;
+
+            // Check to see if last Event from input file
+            if (i == inputEvents.size() - 1) {
+                finalEvent = true;
+            }
+
+            String packageID = name+"_"+i;
+            RelayPackage relayPackage = createRelayPackage( packageID,inputEvents.get(i), finalEvent);
+            System.out.println("[" + name + "] -- created " + packageID +" to send to scheduler");
+            relay.put(name, relayPackage);
         }
+
+        while (true) {
+            RelayPackage receivedPackage = relay.get(name, systemType);
+            acknowledgePackages.add(receivedPackage);
+            System.out.println("[" + name + "] -- Processing event package: " + receivedPackage);
+
+            if (receivedPackage.lastPackage) {
+                break;
+            }
+        }
+
+        System.out.println("[" + name + "] --" + acknowledgePackages);
+
     }
+
 }
