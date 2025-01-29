43f8b21b4ecbd1e8b9fac1d14d63ff782f2fe832
sarahalsaady <sarahalsaady@cmail.carleton.ca>
2025-01-29 14:52:57 -0500

    add folder for tests

---
+++ src/tests/InputEventTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputEventTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSeverity() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
