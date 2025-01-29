5e1c3ca8016dde373713be04c2c0d9ca1f865581
sarahalsaady <sarahalsaady@cmail.carleton.ca>
2025-01-29 17:46:05 -0500

    add tests for InputEvent

--- src/test/InputEventTest.java
+++ src/test/InputEventTest.java
@@ -1,17 +1,24 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class InputEventTest {
+    private InputEvent event;
 
     @BeforeEach
     void setUp() {
+        event = new InputEvent("11:30", "3", "FIRE_DETECTED", "High");
     }
 
     @Test
     void getSeverity() {
+        assertEquals(Severity.High, event.getSeverity());
     }
 
     @Test
     void testToString() {
+        String expected = "Time: 11:30 Zone: 3 Event Type: FIRE_DETECTED Severity: High";
+        assertEquals(expected, event.toString());
     }
 }
