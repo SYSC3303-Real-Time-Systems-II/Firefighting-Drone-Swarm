b43d570be85db5bc928e80b289fc1ad82baa3c67
sarahalsaady <sarahalsaady@cmail.carleton.ca>
2025-01-31 15:21:28 -0500

    update tests

--- src/test/InputEventTest.java
+++ src/test/InputEventTest.java
@@ -8,12 +8,19 @@ class InputEventTest {
 
     @BeforeEach
     void setUp() {
-        event = new InputEvent("11:30", "3", "FIRE_DETECTED", "High");
+        event = new InputEvent("11:30:00", 3, "FIRE_DETECTED", "High");
     }
 
     @Test
     void getSeverity() {
         assertEquals(Severity.High, event.getSeverity());
+    }
+
+    @Test
+    void setZone(){
+        Zone zone = new Zone(1, new Coordinate(0,0), new Coordinate(700,600));
+        event.setZone(zone);
+        assertEquals(zone, event.zone);
     }
 
     @Test
--- src/test/SchedulerTest.java
+++ src/test/SchedulerTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchedulerTest {
@@ -10,8 +12,8 @@ class SchedulerTest {
     @BeforeEach
     void setUp() {
         scheduler = new Scheduler("Scdlr");
-        highPriorityEvent = new InputEvent("11:30", "3", "FIRE_DETECTED", "High");
-        lowPriorityEvent = new InputEvent("11:30", "2", "FIRE_DETECTED", "Low");
+        highPriorityEvent = new InputEvent("11:30:00", 3, "FIRE_DETECTED", "High");
+        lowPriorityEvent = new InputEvent("11:30:00", 2, "FIRE_DETECTED", "Low");
     }
 
     @Test
@@ -43,21 +45,29 @@ class SchedulerTest {
 
     @Test
     void getRelayMessageEvent() {
-        InputEvent event3 = new InputEvent("12:30", "1", "FIRE_DETECTED", "Low");
+        InputEvent event3 = new InputEvent("12:30:00", 1, "FIRE_DETECTED", "Low");
         scheduler.addRelayMessageEvents(highPriorityEvent, Systems.Scheduler, "Test");
         scheduler.addRelayMessageEvents(lowPriorityEvent, Systems.Scheduler, "Test");
         scheduler.addRelayMessageEvents(event3, Systems.Scheduler, "Test");
 
         // Check that events are retrieved in FIFO order
-        assertEquals(highPriorityEvent, scheduler.getRelayMessageEvent(Systems.Scheduler, "Test"));
-        assertEquals(lowPriorityEvent, scheduler.getRelayMessageEvent(Systems.Scheduler, "Test"));
-        assertEquals(event3, scheduler.getRelayMessageEvent(Systems.Scheduler, "Test"));
+        assertEquals(highPriorityEvent, scheduler.getRelayMessageEvent(Systems.Scheduler, "Test", true));
+        assertEquals(lowPriorityEvent, scheduler.getRelayMessageEvent(Systems.Scheduler, "Test", false));
+        assertEquals(event3, scheduler.getRelayMessageEvent(Systems.Scheduler, "Test", false));
 
         // Queue should be empty now
         assertTrue(scheduler.getRelayMessageEvents().isEmpty());
     }
 
     @Test
-    void run() {
+    void addZones() {
+        ArrayList<Zone> zonesList = new ArrayList<>();
+        zonesList.add(new Zone(1, new Coordinate(0,0), new Coordinate(700,600)));
+        zonesList.add(new Zone(2, new Coordinate(0,600), new Coordinate(650,1500)));
+
+        scheduler.addZones(zonesList, Systems.Scheduler, "Test");
+
+        assertEquals(2, scheduler.getZones().size());
+
     }
 }
