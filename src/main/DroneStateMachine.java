61a2829996a9d267d5a04b5586c7382f23c1b8e5
tharu <tharusha.herath13@gmail.com>
2025-03-14 12:42:28 -0400

    Added drone state machine state pattern class.

--- src/main/DroneStateMachine.java
+++ src/main/DroneStateMachine.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class DroneStateMachine {
+interface DroneBaseState{
+    void hande(DroneBaseState context);
+    void addNewTask(InputEvent event);
+}
+
+class InBaseState implements DroneBaseState {
+
+    @Override
+    public void hande(DroneBaseState context) {
+
+    }
+
+    @Override
+    public void addNewTask(InputEvent event) {
+
+    }
+
+
+}
+
+class InFieldState implements DroneBaseState{
+
+    @Override
+    public void hande(DroneBaseState context) {
+
+    }
+
+    @Override
+    public void addNewTask(InputEvent event) {
+
+    }
+}
+
+class AvailableState extends InBaseState {
+
+}
+
+class AscendingState extends InBaseState {
+
+}
+
+class CruisingState extends InFieldState {
+
+}
+
+class ReCalculatingState extends InFieldState {
+
+}
+
+class DropAgentState extends InFieldState {
+
 }
